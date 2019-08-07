b151d0aba7e1c7af33c1ba49096f8b841e096547
Philip McGuire <philip@Philips-MacBook-Pro.local>
2019-08-06 20:39:11 -0400

    cleaning up code

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ enum Token {
     Duplicate,
     ClearAll,
     Pop,
+    Help,
     // TODO:
     // Log,
     // RollUp,
@@ -23,11 +24,8 @@ enum Token {
 
 
 fn main() {
-    print!("{}[2J", 27 as char);
-    println!("Welcome to the Rust RPN Calculator!\n");
-    
+    welcome();
     let mut stack: Vec<f64> = Vec::new();
-  
     loop {
         let tokens = parse_input(get_input());
         apply_tokens(tokens, &mut stack);
@@ -38,11 +36,9 @@ fn main() {
 fn get_input() -> String {
     print!("> ");
     io::stdout().flush().unwrap();
-
     let mut input = String::new();
     io::stdin().read_line(&mut input)
         .expect("Failed to read line!");
-
     input
 }
 
@@ -52,8 +48,8 @@ fn parse_input(input: String) -> Vec<Token> {
 
     for seg in split {
         match seg {
-            "+" => { tokens.push(Token::Add) },
             "-" => { tokens.push(Token::Subtract) },
+            "+" => { tokens.push(Token::Add) },
             "*" => { tokens.push(Token::Multiply) },
             "/" => { tokens.push(Token::Divide) },
 
@@ -67,6 +63,7 @@ fn parse_input(input: String) -> Vec<Token> {
             "d" | "duplicate" => { tokens.push(Token::Duplicate) },
             "rm" | "remove" => { tokens.push(Token::Pop) },
             
+            "h" | "help" => { tokens.push(Token::Help) }
             // Raw number
             _ => {
                 match seg.parse() {
@@ -96,8 +93,8 @@ fn apply_tokens(tokens: Vec<Token>, stack: &mut Vec<f64>) {
             },
             Token::ClearAll => {
                 stack.clear();
+                welcome();
             },
-
             Token::Add => {
                 let a = stack.pop().unwrap();
                 let b = stack.pop().unwrap();
@@ -118,7 +115,6 @@ fn apply_tokens(tokens: Vec<Token>, stack: &mut Vec<f64>) {
                 let b = stack.pop().unwrap();
                 stack.push( b / a );
             },
-
             Token::Pow => {
                 let a = stack.pop().unwrap();
                 let b = stack.pop().unwrap();
@@ -129,9 +125,8 @@ fn apply_tokens(tokens: Vec<Token>, stack: &mut Vec<f64>) {
                 let b = stack.pop().unwrap();
                 stack.push( b.powf(1.0/a));
             },
-
             Token::Floor => {
-                let mut a = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
                 if a < 0.0 {
                     stack.push(a.floor() + 1.0)
                 } else {
@@ -139,26 +134,52 @@ fn apply_tokens(tokens: Vec<Token>, stack: &mut Vec<f64>) {
                 }
             },
             Token::Ceiling => {
-                let mut a = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
                 if a < 0.0 {
                     stack.push(a.floor() - 1.0)
                 } else {
                     stack.push(a.floor() + 1.0);
                 }
             },
-
-            _ => { eprintln!("Unrecognized: {:?}", token) }
+            Token::Help => {
+                print_help();
+            }
         }
     }
 }
 
 fn print_stack(stack: &Vec<f64>) {
-    let mut x: isize = (stack.len() - 1).try_into().unwrap(); 
-
-    for elem in stack.iter() {
-        println!("|[{}]: {}", x, elem);
-        x -= 1;
+    if stack.len() > 0 {
+        let mut x: isize = (stack.len() - 1).try_into().unwrap(); 
+        for elem in stack.iter() {
+            println!("|[{}]: {}", x, elem);
+            x -= 1;
+        }
+        print!("----------------------\n");
     }
+}
 
-    print!("----------------------\n");
+fn print_help() {
+    println!("Help:");
+    println!("--------------------------------------------");
+    println!("Any number gets put on the top of the stack, \nindicated by [0]\n");
+    println!("Op\t\tFunction");
+    println!("--\t\t--------");
+    println!("+\t\tAdd");
+    println!("-\t\tSubtract");
+    println!("*\t\tMultiply");
+    println!("/\t\tDivide");
+    println!("pow\t\tPow ([1]^[0])");
+    println!("root\t\tRoot ([1] root [0]) ");
+    println!("f\t\tFloor");
+    println!("c\t\tCeiling");
+    println!("d\t\tDuplicate");
+    println!("clear\t\tClearAll");
+    println!("rm\t\tPop");
+    println!("--------------------------------------------");
+}
+
+fn welcome() {
+    print!("{}[2J", 27 as char);
+    println!("Welcome to the Rust RPN Calculator!\n");
 }
